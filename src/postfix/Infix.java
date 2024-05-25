b13233066596a343cfa509ace685fecc0cc0a9bd
Waed Madhaji <waed.madhaji@gmail.com>
2024-05-25 16:30:18 +0200

    Implement InfixToPostfix

--- src/postfix/Infix.java
+++ src/postfix/Infix.java
@@ -1,8 +1,72 @@
 package postfix;
 
+import java.util.Stack;
+
 public class Infix {
 
-    public String toPostfix(String infix)  {
-        return "implement infix to postfix here";
+    public String toPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+
+            // If the character is a digit, add it to the output
+            if (Character.isDigit(c)) {
+                postfix.append(c);
+            }
+            // If the character is '(', push it to the stack
+            else if (c == '(') {
+                stack.push(c);
+            }
+            // If the character is ')', pop and output from the stack until '(' is found
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                stack.pop(); // Remove '(' from stack
+            }
+            // If the character is an operator
+            else if (isOperator(c)) {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+
+        // Pop all the operators from the stack
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+
+        return postfix.toString();
+    }
+
+    private boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+    }
+
+    private int precedence(char c) {
+        switch (c) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Infix infixConverter = new Infix();
+        String infixExpression = "3+5*(10-4)";
+        String postfixExpression = infixConverter.toPostfix(infixExpression);
+        System.out.println("Infix: " + infixExpression);
+        System.out.println("Postfix: " + postfixExpression);
     }
 }
